d54ac60bb90fe17eab9c6858f96a5c1046a0b971
Ardia Regita Pramesti <ardiaregitap@gmail.com>
2016-05-05 23:40:16 -0700

    155150200111238_ArdiaRegitaPramesti

--- BangunGeometri.java
+++ BangunGeometri.java
@@ -3,12 +3,6 @@ abstract public class BangunGeometri {
 private String color;
 public BangunGeometri(){
 }
-//public void setColor(String color){
-//    this.color = color;
-//}
-//public String getColor(){
-//    return color;
-//}
 abstract public String color();
 abstract public double Luas();
 abstract public double Keliling();
